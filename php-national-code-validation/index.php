9f6b525f597985e1e0c0205411e2b22fc1729abc
morteza_havangi <mhavangi@ymail.com>
2023-11-26 23:53:35 +0330

    update index.php

--- php-national-code-validation/index.php
+++ php-national-code-validation/index.php
@@ -3,10 +3,10 @@
 include('header.php');
 
 function validateCodeMeli($codeMeli) 
-{
-    $codeMeli = str_replace(['-', '_', ' '], '', $codeMeli); // حذف هر چیزی غیر از ارقام از کد ملی
-
-    if (strlen($codeMeli) != 10) { // بررسی طول کد ملی
+{   // حذف هر چیزی غیر از ارقام از کد ملی
+    $codeMeli = str_replace(['-', '_', ' '], '', $codeMeli); 
+    // بررسی طول کد ملی
+    if (strlen($codeMeli) != 10) { 
         return false;
     }
     // محاسبه جمع ارقام ضرب شده در وزن‌ها
@@ -14,15 +14,14 @@ function validateCodeMeli($codeMeli)
     for ($i = 0; $i < 9; $i++) {
         $sum += $codeMeli[$i] * (10 - $i);
     }
-    // $control = ($sum % 11 < 2) ? $sum % 11 : 11 - ($sum % 11); 
     // محاسبه رقم کنترل
     if ($sum % 11 < 2) {
         $control = $sum % 11;
     } else {
         $control = 11 - ($sum % 11);
     }
-
-    return $control == $codeMeli[9]; // بررسی صحت رقم کنترل
+    // بررسی صحت رقم کنترل
+    return $control == $codeMeli[9]; 
 }
 
 // مثال برای تست
