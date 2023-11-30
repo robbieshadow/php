601e04155f495d4002c7f35ca67127b434974c8b
morteza_havangi <mhavangi@ymail.com>
2023-11-30 16:06:14 +0330

    Update custom-rsort-sort.php

--- customize sort-rsort-implode-explode/custom-rsort-sort.php
+++ customize sort-rsort-implode-explode/custom-rsort-sort.php
@@ -1,20 +1,24 @@
 <?php
-function myRsort(&$array) {
+$numbers = [10, 5, 8, 2, 7];
+
+function myRsort($array) {
     usort($array, function($a, $b) {
-        return $b - $a; 
+        return $b - $a;
     });
+    return $array;
 }
 
-function mySort(&$array) {
+function mySort($array) {
     usort($array, function($a, $b) {
         return $a - $b;
     });
+    return $array;
 }
 
-//exampele
-$numbers = [10, 5, 8, 2, 7];
-myRsort($numbers);
-print_r($numbers);
-echo "<br>";
-mySort($numbers);
+$sortedDesc = myRsort($numbers);
+print_r($sortedDesc);
+
+$sortedAsc = mySort($numbers);
+print_r($sortedAsc);
+
 print_r($numbers);
